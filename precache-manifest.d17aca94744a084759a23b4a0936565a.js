6d37136f27af4639a33b74eb44fe987799a3dd35
Louie <narajamartin@gmail.com>
2020-06-21 00:15:23 +0800

    Updates

---
+++ precache-manifest.d17aca94744a084759a23b4a0936565a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba3434b351fa0b3f4c84c887a190664",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "6274fa84385ef47bdbc4",
+    "url": "/tictactoe/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "788bf36ecea4c4a333c0",
+    "url": "/tictactoe/static/js/2.dc270b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.dc270b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6274fa84385ef47bdbc4",
+    "url": "/tictactoe/static/js/main.6ce58022.chunk.js"
+  },
+  {
+    "revision": "1d8dada0581fd2a39a6b",
+    "url": "/tictactoe/static/js/runtime-main.d9939438.js"
+  }
+]);
